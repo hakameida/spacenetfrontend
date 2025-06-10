6dd049bc59a979bcc1e508bfaef33b729b860fb1
hakameida <hakameida@gmail.com>
2025-06-10 11:28:57 +0300

    adding the computer part and every thing related

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   const pathname = usePathname();
 
 
-  const navLinks = [{ name: "اكسسوارات", href: `/accessories` }, { name: "موبايلات", href: `/mobiles` }, { name: "لابتوبات", href: `/` }];
+  const navLinks = [{ name: "اكسسوارات", href: `/accessories` }, { name: "موبايلات", href: `/mobiles` }, { name: "لابتوبات", href: `/` },{naame : "كومبيوتر", href:'/computer'}];
 
   return (
     <nav className="sticky top-0 left-0 w-full h-[70px] bg-white overflow-hidden shadow-md z-50">
@@ -24,6 +24,14 @@ const Navbar = () => {
           >
             <li className="text-[1rem] font-[400]">
               لابتوبات
+            </li>
+          </a>
+          <a
+            href={'/computer'}
+            className={`${pathname == "/computer" ? "active-link" : "link"}`}
+          >
+            <li className="text-[#000] text-[1rem] font-[400]">
+              كومبيوتر
             </li>
           </a>
           <a
@@ -59,7 +67,7 @@ const Navbar = () => {
         </ul>
         <Link href={"/"} className="flex items-center gap-2">
           <h4 className="text-[16px] md:text-[22px] font-[700] md:text-left text-center font-sans">
-            .Space Net For Laptop
+            Space Net Laptop 
           </h4>
           <img src="/logo.png" alt="icon" width={"60px"} />
         </Link>
